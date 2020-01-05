a8f7db47bed5947ac57c316502952d6b387b42ed
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:04:20 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,2 +1,3 @@
+export declare const b__ctx__store__global: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __ctx__store__global: import("svelte/store").Writable<any>;
 export declare let ctx__store__global: any;
