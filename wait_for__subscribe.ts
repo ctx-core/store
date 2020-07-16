6ecfbfa039689b6ce7c1f6079b8cfb6b264ac599
Brian Takita <brian.takita@gmail.com>
2020-07-16 00:09:10 -0700

    fix: wait_for__subscribe: Promise<T>

--- wait_for__subscribe.ts
+++ wait_for__subscribe.ts
@@ -1,7 +1,7 @@
 import { Readable } from 'svelte/store'
 import { subscribe } from './subscribe'
-export function wait_for__subscribe<T=any>(store: Readable<T>, condition_fn = val=>val) {
-	return new Promise(resolve=>{
+export function wait_for__subscribe<T>(store: Readable<T>, condition_fn = val=>val) {
+	return new Promise<T>(resolve=>{
 		const unsubscribe = subscribe<T>(store, val=>{
 			if (condition_fn(val)) {
 				setTimeout(() => unsubscribe())
