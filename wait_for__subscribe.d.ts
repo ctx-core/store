87e8a2a122045e80ecfcf95aa4ae304fe48eff20
Brian Takita <brian.takita@gmail.com>
2020-07-16 00:13:43 -0700

    gitHead

--- wait_for__subscribe.d.ts
+++ wait_for__subscribe.d.ts
@@ -1,2 +1,2 @@
 import { Readable } from 'svelte/store';
-export declare function wait_for__subscribe<T = any>(store: Readable<T>, condition_fn?: (val: any) => any): Promise<unknown>;
+export declare function wait_for__subscribe<T>(store: Readable<T>, condition_fn?: (val: any) => any): Promise<T>;
