25ba19ecfc9f89c50f62e688052831d4055f2a87
Brian Takita <brian.takita@gmail.com>
2020-07-15 19:07:15 -0700

    fix: wait_for__subscribe: condition is met on initialization: runtime

--- wait_for__subscribe.ts
+++ wait_for__subscribe.ts
@@ -3,7 +3,7 @@ export function wait_for__subscribe(store, condition_fn = val=>val) {
 	return new Promise(resolve=>{
 		const unsubscribe = subscribe(store, val=>{
 			if (condition_fn(val)) {
-				unsubscribe()
+				setTimeout(unsubscribe)
 				resolve(val)
 			}
 		})
