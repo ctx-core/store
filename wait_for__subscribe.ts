be5ae9b38ffa5ca685eddab7f2cdc7f8bbbde733
Brian Takita <brian.takita@gmail.com>
2020-07-15 19:34:31 -0700

    fix: wait_for__subscribe: unsubscribe: runtime

--- wait_for__subscribe.ts
+++ wait_for__subscribe.ts
@@ -3,7 +3,7 @@ export function wait_for__subscribe(store, condition_fn = val=>val) {
 	return new Promise(resolve=>{
 		const unsubscribe = subscribe(store, val=>{
 			if (condition_fn(val)) {
-				setTimeout(unsubscribe)
+				setTimeout(() => unsubscribe())
 				resolve(val)
 			}
 		})
