de139b026e42df9f7a9ccf3260de30e8825bf34c
Brian Takita <brian.takita@gmail.com>
2020-07-28 02:00:24 -0700

    single underscore @ctx-core/array functions

--- clear__store.ts
+++ clear__store.ts
@@ -1,4 +1,4 @@
-import { _a1__wrap } from '@ctx-core/function'
+import { _wrap_a1 } from '@ctx-core/function'
 import { each } from '@ctx-core/array'
 import { _set__val } from './_set__val'
 import { Stores } from './Stores'
@@ -8,6 +8,6 @@ import { Stores } from './Stores'
  * @param val
  */
 export function clear__store<S extends Stores>(stores: S, val = null) {
-	const store_a1 = _a1__wrap(stores)
+	const store_a1 = _wrap_a1(stores)
 	each(store_a1, _set__val(val))
 }
