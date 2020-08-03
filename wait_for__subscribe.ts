ff7b736f85ce09efbc9a30d94b6c94aa4390c983
Brian Takita <brian.takita@gmail.com>
2020-08-03 07:58:07 -0700

    fix: wait_for__subscribe: handle race condition causing multiple unsubscribe calls

--- wait_for__subscribe.ts
+++ wait_for__subscribe.ts
@@ -1,13 +1,14 @@
 import { Readable } from 'svelte/store'
-export function wait_for__subscribe<T>(store: Readable<T>, condition_fn: (T)=>any = (val:T)=>val) {
+export function wait_for__subscribe<T>(store:Readable<T>, condition_fn:(T)=>any = (val:T)=>val) {
 	return new Promise<T>(resolve=>{
-		let unsubscribe: () => void
+		let unsubscribe:()=>void, unsubscribe_oninit:boolean
 		unsubscribe = store.subscribe((val:T)=>{
 			if (condition_fn(val)) {
-				if (unsubscribe) unsubscribe();
-				else setTimeout(() => unsubscribe())
+				if (unsubscribe) unsubscribe()
+				else unsubscribe_oninit = true
 				resolve(val)
 			}
 		})
+		if (unsubscribe_oninit) unsubscribe()
 	})
 }
